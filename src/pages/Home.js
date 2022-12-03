583f15ae92a469508ba44f3a305740a318cd968f
Fahadmohammad1 <205fahadmohammad@gmail.com>
2022-12-03 22:54:11 +0600

    product displayed in homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import ProductCard from "../components/ProductCard";
 
 const Home = () => {
-  return <div></div>;
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("products.json")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10">
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product} />
+      ))}
+    </div>
+  );
 };
 
 export default Home;
