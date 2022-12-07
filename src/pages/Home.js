ad388db2ea464793068909693119661fd9172f49
Fahadmohammad1 <205fahadmohammad@gmail.com>
2022-12-07 19:26:03 +0600

    navbar changed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,14 +13,29 @@ const Home = () => {
       .then((data) => setProducts(data));
   }, []);
 
+  const activeClass = "text-white bg-indigo-500 border-white";
+
   const state = useSelector((state) => state);
   console.log(state);
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10">
-      {products.map((product) => (
-        <ProductCard key={product.id} product={product} />
-      ))}
+    <div className="max-w-7xl gap-14 mx-auto my-10">
+      <div className="mb-10 flex justify-end gap-5">
+        <button className="border px-3 py-2 rounded-full font-semibold">
+          In Stock
+        </button>
+        <button className="border px-3 py-2 rounded-full font-semibold">
+          AMD
+        </button>
+        <button className="border px-3 py-2 rounded-full font-semibold">
+          Intel
+        </button>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10">
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
     </div>
   );
 };
