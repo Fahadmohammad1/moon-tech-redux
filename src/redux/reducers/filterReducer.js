8e4cdd57b5269ce2c4028e446f98d322fd154b1f
Fahadmohammad1 <205fahadmohammad@gmail.com>
2022-12-07 20:38:56 +0600

    filter reducer added

--- src/redux/reducers/filterReducer.js
+++ src/redux/reducers/filterReducer.js
@@ -1,7 +1,22 @@
-const initialState = {};
+import { TOGGLE_BRAND, TOGGLE_STOCK } from "../actionTypes/actionTypes";
+
+const initialState = {
+  filters: {
+    brand: [],
+    stock: false,
+  },
+  keyword: "",
+};
 
 const filterReducer = (state = initialState, action) => {
-  return state;
+  switch (action.type) {
+    case TOGGLE_BRAND:
+      return {};
+    case TOGGLE_STOCK:
+      return {};
+    default:
+      return state;
+  }
 };
 
 export default filterReducer;
